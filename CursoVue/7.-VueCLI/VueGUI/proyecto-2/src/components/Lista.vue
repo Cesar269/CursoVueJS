886bbbf87ea2caf965b9ff782f0a99abfe386acf
Cesar269 <cesarin-268@hotmail.com>
2020-09-02 13:48:20 -0500

    Curso hasta la primer practica con CLI

---
+++ CursoVue/7.-VueCLI/VueGUI/proyecto-2/src/components/Lista.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li v-for="(item,i) of ordenamiento" :key="item.id" @click="aumentar(i)" class="list-group-item d-flex justify-content-between align-items-center">
+        {{i+1}} - {{item.nombre}}
+        <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+      </li>
+    </ul>
+    <button class="btn btn-danger btn-block " @click="reiniciar()">Reiniciar</button>
+  </div>
+</template>
+<script>
+import { mapState,mapMutations } from "vuex";
+export default {
+  name: "Lista",
+  computed: {
+    ...mapState(["frutas"]),
+    ordenamiento(){
+        return this.frutas.sort((a,b)=>b.cantidad - a.cantidad)
+    }
+  },
+  methods: {
+      ...mapMutations(['aumentar','reiniciar'])
+  },
+};
+</script>
